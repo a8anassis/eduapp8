e296aad56b00c2ce54fe15d803dac4c348e8d9fd
a8anassis <a8anassis@gmail.com>
2025-07-30 19:04:56 +0300

    fix: add find by identity number

--- src/main/java/gr/aueb/cf/eduapp/repository/PersonalInfoRepository.java
+++ src/main/java/gr/aueb/cf/eduapp/repository/PersonalInfoRepository.java
@@ -1,6 +1,7 @@
 package gr.aueb.cf.eduapp.repository;
 
 import gr.aueb.cf.eduapp.model.PersonalInfo;
+import gr.aueb.cf.eduapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -10,4 +11,5 @@ public interface PersonalInfoRepository extends JpaRepository<PersonalInfo, Long
         JpaSpecificationExecutor<PersonalInfo> {
 
     Optional<PersonalInfo> findByAmka(String amka);
+    Optional<PersonalInfo> findByIdentityNumber(String identityNumber);
 }
