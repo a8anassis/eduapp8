26927a64094709cab16809b5d2e714af80b79935
a8anassis <a8anassis@gmail.com>
2025-07-31 21:14:24 +0300

    fix: clear

--- src/main/java/gr/aueb/cf/eduapp/repository/UserRepository.java
+++ src/main/java/gr/aueb/cf/eduapp/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package gr.aueb.cf.eduapp.repository;
 
 import gr.aueb.cf.eduapp.model.User;
+import org.springframework.data.convert.ReadingConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>,
         JpaSpecificationExecutor<User> {
 
--- src/main/java/gr/aueb/cf/eduapp/security/JwtAuthenticationFilter.java
+++ src/main/java/gr/aueb/cf/eduapp/security/JwtAuthenticationFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.NonNull;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -30,7 +31,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final UserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         final String authHeader = request.getHeader("Authorization");
