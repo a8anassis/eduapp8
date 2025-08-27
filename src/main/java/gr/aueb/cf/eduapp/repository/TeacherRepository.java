99bc7c55610f01b6f1c28af470997729b8a9d846
a8anassis <a8anassis@gmail.com>
2025-08-27 18:10:25 +0300

    fix: remove comments

--- src/main/java/gr/aueb/cf/eduapp/repository/TeacherRepository.java
+++ src/main/java/gr/aueb/cf/eduapp/repository/TeacherRepository.java
@@ -4,14 +4,15 @@ import gr.aueb.cf.eduapp.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long>,
         JpaSpecificationExecutor<Teacher> {
 
-    // UserId by convention looks for the @Id field of User entity since
-    // we have a @OneToOne relation
     Optional<Teacher> findByUserId(Long id);
 
     Optional<Teacher> findByUuid(String uuid);
+
+    List<Teacher> findByUserLastname(String lastname);
 }
