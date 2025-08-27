6994e528e6677c54c6105e1a21c010628aa94e16
a8anassis <a8anassis@gmail.com>
2025-08-27 18:05:52 +0300

    fix: add map to teacher entity from update dto

--- src/main/java/gr/aueb/cf/eduapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/eduapp/mapper/Mapper.java
@@ -15,25 +15,15 @@ public class Mapper {
     private final PasswordEncoder passwordEncoder;
 
     public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
-        TeacherReadOnlyDTO teacherReadOnlyDTO = new TeacherReadOnlyDTO();
-        teacherReadOnlyDTO.setId(teacher.getId());
-        teacherReadOnlyDTO.setUuid(teacher.getUuid());
-        teacherReadOnlyDTO.setIsActive(teacher.getIsActive());
 
-        // Map User to UserReadOnlyDTO
-        UserReadOnlyDTO userDTO = new UserReadOnlyDTO();
-        userDTO.setFirstname(teacher.getUser().getFirstname());
-        userDTO.setLastname(teacher.getUser().getLastname());
-        userDTO.setVat(teacher.getUser().getVat());
-        teacherReadOnlyDTO.setUserReadOnlyDTO(userDTO);
+        UserReadOnlyDTO userReadOnlyDTO = new UserReadOnlyDTO(teacher.getUser().getFirstname(),
+                teacher.getUser().getLastname(), teacher.getUser().getVat());
 
-        // Map PersonalInfo to PersonalInfoReadOnlyDTO
-        PersonalInfoReadOnlyDTO personalInfoDTO = new PersonalInfoReadOnlyDTO();
-        personalInfoDTO.setAmka(teacher.getPersonalInfo().getAmka());
-        personalInfoDTO.setIdentityNumber(teacher.getPersonalInfo().getIdentityNumber());
-        teacherReadOnlyDTO.setPersonalInfoReadOnlyDTO(personalInfoDTO);
+        PersonalInfoReadOnlyDTO personalInfoReadOnlyDTO = new PersonalInfoReadOnlyDTO(teacher.getPersonalInfo().getAmka(),
+                teacher.getPersonalInfo().getIdentityNumber());
 
-        return teacherReadOnlyDTO;
+        return new TeacherReadOnlyDTO(teacher.getId(), teacher.getUuid(),
+                teacher.getIsActive(), userReadOnlyDTO, personalInfoReadOnlyDTO );
     }
 
 
@@ -41,7 +31,6 @@ public class Mapper {
         Teacher teacher = new Teacher();
         teacher.setIsActive(dto.isActive());
 
-        // Map fields from UserDTO
         UserInsertDTO userDTO = dto.userInsertDTO();
         User user = new User();
         user.setFirstname(userDTO.firstname());
@@ -59,7 +48,6 @@ public class Mapper {
         user.setIsActive(dto.isActive());
         teacher.setUser(user);  // Set User entity to Teacher
 
-        // Map fields from PersonalInfoDTO
         PersonalInfoInsertDTO personalInfoDTO = dto.personalInfoInsertDTO();
         PersonalInfo personalInfo = new PersonalInfo();
         personalInfo.setAmka(personalInfoDTO.amka());
@@ -71,4 +59,59 @@ public class Mapper {
 
         return teacher;
     }
+
+    public Teacher mapToTeacherEntity(TeacherUpdateDTO dto) {
+        Teacher teacher = new Teacher();
+        teacher.setId(dto.id());
+        teacher.setIsActive(dto.isActive());
+        teacher.setUuid(dto.uuid());
+
+        UserUpdateDTO userDTO = dto.userUpdateDTO();
+        User user = new User();
+        user.setId(userDTO.id());
+        user.setFirstname(userDTO.firstname());
+        user.setLastname(userDTO.lastname());
+        user.setUsername(userDTO.username());
+        user.setPassword(passwordEncoder.encode(userDTO.password()));
+        user.setVat(userDTO.vat());
+        user.setFatherName(userDTO.fatherName());
+        user.setFatherLastname(userDTO.fatherLastname());
+        user.setMotherName(userDTO.motherName());
+        user.setMotherLastname(userDTO.motherLastname());
+        user.setDateOfBirth(userDTO.dateOfBirth());
+        user.setGender(userDTO.gender());
+        user.setRole(userDTO.role());
+        user.setIsActive(dto.isActive());
+        teacher.setUser(user);  // Set User entity to Teacher
+
+        PersonalInfoUpdateDTO personalInfoDTO = dto.personalInfoUpdateDTO();
+        PersonalInfo personalInfo = new PersonalInfo();
+        personalInfo.setId(personalInfoDTO.id());
+        personalInfo.setAmka(personalInfoDTO.amka());
+        personalInfo.setIdentityNumber(personalInfoDTO.identityNumber());
+        personalInfo.setPlaceOfBirth(personalInfoDTO.placeOfBirth());
+        personalInfo.setMunicipalityOfRegistration(personalInfoDTO
+                .municipalityOfRegistration());
+        teacher.setPersonalInfo(personalInfo);  // Set PersonalInfo entity to Teacher
+
+        return teacher;
+    }
 }
+
+//        TeacherReadOnlyDTO teacherReadOnlyDTO = new TeacherReadOnlyDTO();
+//        teacherReadOnlyDTO.setId(teacher.getId());
+//        teacherReadOnlyDTO.setUuid(teacher.getUuid());
+//        teacherReadOnlyDTO.setIsActive(teacher.getIsActive());
+
+//        userDTO.setFirstname(teacher.getUser().getFirstname());
+//        userDTO.setLastname(teacher.getUser().getLastname());
+//        userDTO.setVat(teacher.getUser().getVat());
+//        teacherReadOnlyDTO.setUserReadOnlyDTO(userDTO);
+
+//        personalInfoDTO.setAmka(teacher.getPersonalInfo().getAmka());
+//        personalInfoDTO.setIdentityNumber(teacher.getPersonalInfo().getIdentityNumber());
+//        teacherReadOnlyDTO.setPersonalInfoReadOnlyDTO(personalInfoDTO);
+//
+//        TeacherReadOnlyDTO teacherReadOnlyDTO = new TeacherReadOnlyDTO(teacher.getId(), teacher.getUuid(), teacher.getIsActive(), userDTO, personalInfoDTO );
+//
+//        return teacherReadOnlyDTO;
