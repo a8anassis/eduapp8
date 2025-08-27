4009ca1af9492ecfd0a2408c6083f028f363e323
a8anassis <a8anassis@gmail.com>
2025-08-27 18:09:00 +0300

    fix: change to record from Lombok

--- src/main/java/gr/aueb/cf/eduapp/dto/ResponseMessageDTO.java
+++ src/main/java/gr/aueb/cf/eduapp/dto/ResponseMessageDTO.java
@@ -1,16 +1,8 @@
 package gr.aueb.cf.eduapp.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class ResponseMessageDTO {
-    private String code;
-    private String description;
+public record ResponseMessageDTO(String code, String description) {
 
     public ResponseMessageDTO(String code) {
-        this.code = code;
-        this.description = "";
+        this(code, "");     // Calls the canonical constructor
     }
 }
