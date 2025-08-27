5f2ef23ffa649abae0ac985900dd98d8da8bb65f
a8anassis <a8anassis@gmail.com>
2025-08-27 17:58:01 +0300

    feat: add find-by-uuid

--- src/main/java/gr/aueb/cf/eduapp/repository/EmployeeRepository.java
+++ src/main/java/gr/aueb/cf/eduapp/repository/EmployeeRepository.java
@@ -4,7 +4,10 @@ import gr.aueb.cf.eduapp.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long>,
         JpaSpecificationExecutor<Employee> {
 
+    Optional<Employee> findByUuid(String uuid);
 }
