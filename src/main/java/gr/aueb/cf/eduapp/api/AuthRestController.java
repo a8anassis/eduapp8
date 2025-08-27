f8771c815c9df62dbaa0da5232e2f96aca955271
a8anassis <a8anassis@gmail.com>
2025-08-27 17:54:49 +0300

    feat: add Swagger documentation

--- src/main/java/gr/aueb/cf/eduapp/api/AuthRestController.java
+++ src/main/java/gr/aueb/cf/eduapp/api/AuthRestController.java
@@ -4,6 +4,10 @@ import gr.aueb.cf.eduapp.authentication.AuthenticationService;
 import gr.aueb.cf.eduapp.dto.AuthenticationRequestDTO;
 import gr.aueb.cf.eduapp.dto.AuthenticationResponseDTO;
 import gr.aueb.cf.eduapp.dto.ResponseMessageDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +23,22 @@ public class AuthRestController {
 
     private final AuthenticationService authenticationService;
 
+    @Operation(
+            summary = "Authenticate user",
+            description = "Returns JWT token for valid credentials",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Authentication successful",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthenticationResponseDTO.class))),
+                    @ApiResponse(
+                            responseCode = "401", description = "Unauthorized - Invalid credentials",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "400",  description = "Bad request - Missing/invalid parameters",
+                            content = @Content)
+            }
+    )
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponseDTO> authenticate(@RequestBody AuthenticationRequestDTO authenticationRequestDTO) {
         AuthenticationResponseDTO authenticationResponseDTO = authenticationService.authenticate(authenticationRequestDTO);
