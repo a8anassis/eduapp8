42cd8921cbb089b91ec001546d6cae8fea1e68a0
a8anassis <a8anassis@gmail.com>
2025-08-27 18:07:17 +0300

    fix: change from Lombok to record

--- src/main/java/gr/aueb/cf/eduapp/dto/PersonalInfoReadOnlyDTO.java
+++ src/main/java/gr/aueb/cf/eduapp/dto/PersonalInfoReadOnlyDTO.java
@@ -1,15 +1,8 @@
 package gr.aueb.cf.eduapp.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Builder;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class PersonalInfoReadOnlyDTO {
-    private String amka;
-    private String identityNumber;
-}
+@Builder
+public record PersonalInfoReadOnlyDTO(String amka, String identityNumber) {}
+
+
