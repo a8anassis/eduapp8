034a257542f560198a7001010dcce8940827b958
a8anassis <a8anassis@gmail.com>
2025-09-04 16:51:20 +0300

    fix: clear

--- src/main/java/gr/aueb/cf/eduapp/core/ErrorHandler.java
+++ src/main/java/gr/aueb/cf/eduapp/core/ErrorHandler.java
@@ -5,13 +5,16 @@ import gr.aueb.cf.eduapp.dto.ResponseMessageDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -26,9 +29,9 @@ import java.util.Map;
 @Slf4j
 public class ErrorHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({ValidationException.class})
+    @ExceptionHandler(ValidationException.class)
     public ResponseEntity<Map<String, String>> handleValidationException(ValidationException e) {
-        log.warn("validation failed. Message={}", e.getMessage(), e);
+        log.warn("validation failed. Message={}", e.getMessage());
         BindingResult bindingResult = e.getBindingResult();
 
         Map<String , String > errors = new HashMap<>();
